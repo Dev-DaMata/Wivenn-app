357674ff8c51a95965f2f039509aa95aa0f0bade
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-08-05 10:15:27 -0300

    Fixed cards data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Cards from "../components/Cards";
 
 function Home() {
   const cardsData = [
-    {title: 'Departamentos', text: 'Administre os seus departamentos', link: '/department'},
+    {title: 'Departamentos', text: 'Administre os seus departamentos', link: '/departments'},
     {title: 'Funcionarios', text: 'Administre os seus funcionarios', link: '/employees'},
     {title: 'Tarefas', text: 'Administre todas as suas tarefas', link: '/tasks'},
   ];

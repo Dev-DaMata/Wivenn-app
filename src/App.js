9062965f90f690b487bb0a66e68d5f1153980ba5
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-08-05 10:17:43 -0300

    Departments route created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Departments from "./pages/Departments"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/departments" element={<Departments />} />
       </Routes>
     </Router>
   );

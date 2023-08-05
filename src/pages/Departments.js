cc7ac0e416c0257c9560f6bb42e569ccf7654dbe
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-08-05 15:17:37 -0300

    navigate to dapartment detail and delete

--- src/pages/Departments.js
+++ src/pages/Departments.js
@@ -3,12 +3,13 @@ import { promisseApi } from "../utils/promisseApi";
 import EditIcon from '@mui/icons-material/Edit';
 import "../styles/Departments.css";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import Modal from "../components/Modal";
+import { useNavigate } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Departaments() {
     const [departments, setDepartments] = useState([])
     const [name, setName] = useState('')
-    const [open, setOpen] = useState(false)
+    const navigate = useNavigate();
 
     const getGrid = () =>{
         let filtro = {};
@@ -30,6 +31,17 @@ function Departaments() {
             }//configs diversas
         )
     }
+
+    const handleDelete=(id)=>{
+        promisseApi(
+            'delete',
+            `departamento/${id}`,
+            (data)=>{
+                console.log(data)
+                getGrid()
+            }
+            )
+    }
     
     useEffect(()=>{
         getGrid()
@@ -40,17 +52,18 @@ function Departaments() {
             <div className="deparment-filter">
                 <input className="department-filter-input" name="Name" type="text" placeholder="Nome do departemento" value={name} onChange={(e)=>{setName(e.target.value)}}/>
                 <button className="department-filter-button" onClick={()=>getGrid()} ><FilterAltIcon fontSize="12" className="department-filter-button-icon"/>Filtrar!</button>
-                <button onClick={()=>setOpen(true)}>Criar Departamento</button>
             </div>
             <div className="department-list">
             {departments.map((department, idx) => {return(
                 <div className={idx % 2 == 0 ? "department-list-item-dark" : "department-list-item-light"} key={idx}>
                     <h1 className="deparment-list-item-title">{department.Name}</h1>
-                    <div className="department-list-item-icon"><EditIcon/></div>
+                    <div className="department-list-item-icon-container" >
+                        <div className="department-list-item-icon" onClick={()=>{handleDelete(department.id)}}><DeleteIcon/></div>
+                        <div className="department-list-item-icon" onClick={()=>{navigate(`/departments/${department.id}`)}}><EditIcon/></div>
+                    </div>
                 </div>
             )})}
             </div>
-            <Modal open={open} setOpen={setOpen}/>
         </div>
     )
 }
